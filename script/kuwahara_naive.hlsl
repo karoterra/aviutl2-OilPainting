719db892c06b643ee592168e67bbfe0d34d96194
karoterra <karoterra2008@gmail.com>
2025-07-12 15:17:40 +0900

    :recycle: シェーダーを少しだけリファクタリング

--- script/kuwahara_naive.hlsl
+++ script/kuwahara_naive.hlsl
@@ -25,13 +25,15 @@ float4 kuwahara_naive(float4 pos : SV_Position) : SV_Target {
     src.GetDimensions(uw, uh);
     int w = int(uw);
     int h = int(uh);
+    int2 bottomRight = int2(w - 1, h - 1);
+    int bs = int(blurSize);
     float n = (blurSize + 1) * (blurSize + 1);
 
-    float2 offset[4] = {
-        float2(-blurSize, -blurSize),
-        float2(0, -blurSize),
-        float2(-blurSize, 0),
-        float2(0, 0)
+    int2 offset[4] = {
+        int2(-bs, -bs),
+        int2(0, -bs),
+        int2(-bs, 0),
+        int2(0, 0)
     };
     float4 color[4] = {
         float4(0, 0, 0, 0),
@@ -50,13 +52,11 @@ float4 kuwahara_naive(float4 pos : SV_Position) : SV_Target {
     for (int i = 0; i < 4; i++) {
         for (int y = 0; y <= blurSize; y++) {
             for (int x = 0; x <= blurSize; x++) {
-                int2 samplePos = ipos + int2(offset[i].x + x, offset[i].y + y);
-                samplePos = clamp(samplePos, int2(0, 0), int2(w - 1, h - 1));
-                float4 sampleColor = src.Load(int3(samplePos, 0));
+                int2 samplePos = clamp(ipos + offset[i] + int2(x, y), int2(0, 0), bottomRight);
+                float4 sampleColor = src[samplePos];
                 color[i] += sampleColor;
                 float v = pixel_value(sampleColor);
-                value[i].x += v;
-                value[i].y += v * v;
+                value[i] += float2(v, v * v);
             }
         }
     }
@@ -64,11 +64,9 @@ float4 kuwahara_naive(float4 pos : SV_Position) : SV_Target {
     // 平均・分散の計算
     float var[4];
     for (int j = 0; j < 4; j++) {
-        value[j].x /= n;
-        value[j].y /= n;
-        var[j] = value[j].y - value[j].x * value[j].x;
-
         color[j] /= n;
+        value[j] /= n;
+        var[j] = value[j].y - value[j].x * value[j].x;
     }
 
     // 最小分散の領域を選択
